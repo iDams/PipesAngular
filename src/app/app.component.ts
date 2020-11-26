0cc8eac2049e43c285fed209f604727bf2d7efe5
Marco Astorga <marcoastorga.g@gmail.com>
2020-11-26 10:39:57 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  nombre: string = 'Marco Astorga';
+  name: string = 'Marco Astorga';
+  array: string[] = [
+    'Angular',
+    'VsCode',
+    'GitHub',
+    'NestJS',
+    'Developer',
+    'Marco',
+    '2020',
+  ];
+  PI: number = Math.PI;
+  percentage: number = 0.234;
 }
